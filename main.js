a8e8af2724e54ca1c6afb15f3a2059aa8e710fe3
Bhavya337 <72799302+Bhavya337@users.noreply.github.com>
2020-12-10 17:01:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+var Canvas=new fabric.Canvas('myCanvas');
+
+player_x=10;
+player_y=10;
+
+block_height=30;
+block_width =30;
+
+var player_object="";
+var block_object="";
+
+function player_update(){
+fabric.Image.fromURL("ADV C86 2.JPG",function (Img){ player_object=Img;
+     player_object.scaleToWidth(150); 
+     player_object.scaleToHeight(150); 
+     player_object.set({ top:player_y,left:player_x});
+    Canvas.add(player_object);
+    }
+    );
+}
+
+function new_image(get_Image){
+    fabric.Image.fromURL(get_Image,function (Img){ block_object=Img;
+      block_object.scaleToWidth(block_width); 
+        block_object.scaleToHeight(block_height); 
+      block_object.set({ top:player_y,left:player_x});
+       Canvas.add(block_object);
+       }
+       );
+
+}
+
+
+
+
+
